38eb16daab62f8c0a717e3229e697432ba7425d0
Kolyo Sardamov <Kolyo.Sardamov@bulpros.com>
2020-07-15 13:55:36 +0300

    added logging

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 use App\Photo;
 use Illuminate\Support\Facades\Storage;
 use App\StorageGateways\PhotoGateway;
+use Illuminate\Support\Facades\Log;
 
 class PhotosController extends Controller
 {
@@ -45,18 +46,18 @@ class PhotosController extends Controller
             'photo_title' => 'unique:photos'
         ]);
 
-        //$fileName = time().'.'.$request->file->getClientOriginalExtension();
         $file = base64_encode(file_get_contents($request->file('file')->path()));
 
-        // when success is returnd with url from the bewlo request I save it in the db as photo_url
         $url = $this->storage->sendData($file);
-        // the problem here is that the storage returns {"status":"error","message":"Required post data was not sent."}
+
         if($url){
             $photo = new Photo;
             $photo->photo_title = $request->photo_title;
             $photo->photo_url = $url;
             $photo->user_id = Auth::id();
             $photo->save();
+
+            Log::info('User with id '.Auth::id().' has uploaded a new photo with url '.$url);
         }
 
         return response()->json(['success'=>'You have successfully upload file.']);
